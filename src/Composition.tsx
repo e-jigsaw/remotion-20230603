f6e262289f61d6b8dc73624c7044c1679933fa18
jigsaw <m@jgs.me>
2023-06-03 16:29:43 +0900

    go

--- src/Composition.tsx
+++ src/Composition.tsx
@@ -1,3 +1,19 @@
+import { Video, staticFile, Sequence } from 'remotion';
+import { Cc } from './CC';
+
 export const MyComposition = () => {
-  return null;
+  return (
+    <>
+      <Sequence durationInFrames={2653}>
+        <Video src={staticFile('v1.mp4')} style={{ margin: '0 auto' }} />
+      </Sequence>
+      <Sequence from={2653} durationInFrames={3574}>
+        <Video src={staticFile('v2.mp4')} style={{ margin: '0 auto' }} />
+      </Sequence>
+      <Sequence from={6227}>
+        <Video src={staticFile('v3.mp4')} style={{ margin: '0 auto' }} />
+      </Sequence>
+      <Cc />
+    </>
+  );
 };
